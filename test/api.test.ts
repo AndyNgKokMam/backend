50850b6aecf96d411b5cfc8ffba2c5c4aacf3087
AndyNg <andy.ng@s2t.ai>
2021-09-18 17:59:13 +0800

    A

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 import dotenv from 'dotenv'
-dotenv.config({ path: './env' })
+dotenv.config({ path: './env.test' })
 
 import { expect } from 'chai'
 import { before, describe, it } from 'mocha'
